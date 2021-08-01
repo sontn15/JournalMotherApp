bab8e9045d8972e19f67c4311fc7bf9a0f98240a
Tran Ngoc Son <tns.tranngocson@gmail.com>
2021-08-01 21:33:39 +0700

    update

--- app/src/main/java/com/sh/journalmotherapp/ui/main/MainActivity.java
+++ app/src/main/java/com/sh/journalmotherapp/ui/main/MainActivity.java
@@ -46,16 +46,13 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_news, R.id.nav_support, R.id.nav_memory, R.id.nav_account, R.id.nav_setting)
+                R.id.nav_news, R.id.nav_support, R.id.nav_memory, R.id.nav_account)
                 .setDrawerLayout(drawer)
                 .build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-//        NotificationHelper.scheduleRepeatingRTCNotification(this, "2", "19");
-//        NotificationHelper.enableBootReceiver(this);
     }
 
     @Override
